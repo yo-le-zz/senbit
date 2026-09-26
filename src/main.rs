a4f395cfa78f7203181a824d211af3f33dd5a12a
yolezz <yolezz.secret@gmail.com>
2026-09-26 19:48:38 +0200

    adding the mounting filesystem and kpanic macro for make kernelpanic

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+// src/main.rs
+
+use colored::Colorize;
+
+// modules
+mod utils;
+mod kernel;
+mod filesystem;
+
+// imports
+use utils::clear;
+use kernel::panic;
+use filesystem::{fs, disk};
+
+// wrapper pour clear
+fn clear() {
+    if clear::clear_screen().is_err() {
+        clear::clear_screen_ansi();
+    }
+}
+
+fn main() {
+    clear();
+
+    // start different steps
+    println!("{}", "Senbit init starting...".cyan());
+
+    // mount filesystems
+    println!("{}", "Mounting filesystems...".cyan());
+    if let Err(e) = fs::mount_filesystems() {
+        kpanic!(format!("Failed to mount filesystems: {}", e));
+    }
+    println!("{}", "Filesystems mounted successfully.".green());
+
+    // disk detection
+    println!("{}", "Detecting disks...".cyan());
+    disk::disk_detection();
+
+    loop {}
+}
